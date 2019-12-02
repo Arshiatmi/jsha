08587c157eb6bd903f51c1a32e09f37751a25f17
Arshia <35062789+Arshiaprg@users.noreply.github.com>
2019-12-02 22:09:24 +0330

    Fix Lots Of Bugs And add pageController And Some Fixes In menuCreator

--- jsha.js
+++ jsha.js
@@ -101,7 +101,19 @@ function moveY(obj1, how_many) {
 
 // Set Y Of Element In [how_many] Pixels .
 function setY(obj1, how_many) {
-    obj1.style.top = (-how_many) + "px";
+    obj1.style.top = (window.innerHeight - how_many) + "px";
+}
+
+// Set X,Y Of Element In [how_many] Pixels .
+function setL(obj1, x, y) {
+    if (obj1.style.height) {
+        obj1.style.top = (window.innerHeight - y - parseInt(obj1.style.height)) + "px";
+        obj1.style.left = x + "px";
+    }
+    else {
+        obj1.style.top = (window.innerHeight - y) + "px";
+        obj1.style.left = x + "px";
+    }
 }
 
 // Remove An Object .
@@ -110,104 +122,104 @@ function remove(obj1) {
 }
 
 // Get Direction Of Two Objects .
-function getDirection(obj1,obj2){
-    if(obj1.type == "gameObject"){
-        if(obj2.type == "gameObject"){
+function getDirection(obj1, obj2) {
+    if (obj1.type == "gameObject") {
+        if (obj2.type == "gameObject") {
             let x = obj2.getLocation().x - obj1.getLocation().x;
             let y = obj2.getLocation().y - obj1.getLocation().y;
-            return { "x" : x , "y" : y };
+            return { "x": x, "y": y };
         }
-        else{
+        else {
             let x = getLocation(obj2).x - obj1.getLocation().x;
             let y = getLocation(obj2).y - obj1.getLocation().y;
-            return { "x" : x , "y" : y };
+            return { "x": x, "y": y };
         }
     }
-    else{
-        if(obj2.type == "gameObject"){
+    else {
+        if (obj2.type == "gameObject") {
             let x = obj2.getLocation().x - getLocation(obj1).x;
             let y = obj2.getLocation().y - getLocation(obj1).y;
-            return { "x" : x , "y" : y };
+            return { "x": x, "y": y };
         }
-        else{
+        else {
             let x = getLocation(obj2).x - getLocation(obj1).x;
             let y = getLocation(obj2).y - getLocation(obj1).y;
-            return { "x" : x , "y" : y };
+            return { "x": x, "y": y };
         }
     }
 }
 
 // Get String Direction Of Two Objects .
-function getDirectionS(obj1,obj2){
-    let dirs = getDirection(obj1,obj2);
-    if(dirs.x > 0){
-        if(dirs.y > 0){
+function getDirectionS(obj1, obj2) {
+    let dirs = getDirection(obj1, obj2);
+    if (dirs.x > 0) {
+        if (dirs.y > 0) {
             return "UR";
         }
-        else if(dirs.y == 0){
+        else if (dirs.y == 0) {
             return "R";
         }
-        else{
+        else {
             return "DR";
         }
     }
-    else if(dirs.x == 0){
-        if(dirs.y > 0){
+    else if (dirs.x == 0) {
+        if (dirs.y > 0) {
             return "U";
         }
-        else if(dirs.y == 0){
+        else if (dirs.y == 0) {
             return "";
         }
-        else{
+        else {
             return "D";
         }
     }
-    else{
-        if(dirs.y > 0){
+    else {
+        if (dirs.y > 0) {
             return "UL";
         }
-        else if(dirs.y == 0){
+        else if (dirs.y == 0) {
             return "L";
         }
-        else{
+        else {
             return "DL";
         }
     }
 }
 
 // Get Geographical Direction Of Two Objects .
-function getDirectionG(obj1,obj2){
-    let dirs = getDirection(obj1,obj2);
-    if(dirs.x > 0){
-        if(dirs.y > 0){
+function getDirectionG(obj1, obj2) {
+    let dirs = getDirection(obj1, obj2);
+    if (dirs.x > 0) {
+        if (dirs.y > 0) {
             return "NE";
         }
-        else if(dirs.y == 0){
+        else if (dirs.y == 0) {
             return "E";
         }
-        else{
+        else {
             return "SE";
         }
     }
-    else if(dirs.x == 0){
-        if(dirs.y > 0){
+    else if (dirs.x == 0) {
+        if (dirs.y > 0) {
             return "N";
         }
-        else if(dirs.y == 0){
+        else if (dirs.y == 0) {
             return "";
         }
-        else{
+        else {
             return "S";
         }
     }
-    else{
-        if(dirs.y > 0){
+    else {
+        if (dirs.y > 0) {
             return "NW";
         }
-        else if(dirs.y == 0){
+        else if (dirs.y == 0) {
             return "W";
         }
-        else{
+        else {
             return "SW";
         }
     }
@@ -245,26 +257,29 @@ function gameObject(id) {
     this.remove = function () {
         return remove(this.object);
     };
-    this.x = function(){
+    this.x = function () {
         return this.getLocation().x;
     };
-    this.y = function(){
+    this.y = function () {
         return this.getLocation().y;
     };
-    this.setX = function(x){
-        return setX(this.object,x);
+    this.setX = function (x) {
+        return setX(this.object, x);
     };
-    this.setY = function(y){
-        return setY(this.object,y);
+    this.setY = function (y) {
+        return setY(this.object, y);
     };
-    this.getDirection = function(obj2){
-        return getDirection(this.object,obj2);
+    this.setL = function (x, y) {
+        return setL(this.object, x, y);
+    }
+    this.getDirection = function (obj2) {
+        return getDirection(this, obj2);
     };
-    this.getDirectionS = function(obj2){
-        return getDirectionS(this.object,obj2);
+    this.getDirectionS = function (obj2) {
+        return getDirectionS(this, obj2);
     };
-    this.getDirectionG = function(obj2){
-        return getDirectionG(this.object,obj2);
+    this.getDirectionG = function (obj2) {
+        return getDirectionG(this, obj2);
     };
     this.init();
 }
@@ -275,6 +290,9 @@ function getLocation(obj1) {
         if (obj1.type == "gameObject") {
             var a = parseInt(obj1.object.style.left);
             var b = parseInt(obj1.object.style.top);
+        }
+        else {
+            throw "Nothing !";
         }
         return { "x": a, "y": b };
     }
@@ -300,7 +318,7 @@ function audioPlayer(id) {
     this.funcs = {};
     this.object = $(id);
     this.addPlayer = function (event, func) {
-        return this.object.addEventListener(event,func);
+        return this.object.addEventListener(event, func);
     }
 }
 
@@ -311,9 +329,9 @@ function playAudio(source) {
 }
 
 // A Function To Manage Keypressed Events .
-function ctrl(e){
-    for(i in keys){
-        if(e.key == i){
+function ctrl(e) {
+    for (i in keys) {
+        if (e.key == i) {
             keys[i]();
             break;
         }
@@ -324,50 +342,94 @@ function ctrl(e){
 var keys = {};
 
 // A Class To Control The Key Pressed Better That Past .
-function keyController(){
-    this.addKey = function(key,func){
+function keyController() {
+    this.addKey = function (key, func) {
         keys[key] = func;
         return "Succussfully Added !";
     }
-    this.control = function(){
-        sendEvent("keydown",ctrl);
+    this.control = function () {
+        sendEvent("keydown", ctrl);
         return "Controlling ...";
     }
 }
 
 // Create A Menu In A Div Easily .
-function menuCreator(id){
+function menuCreator(id) {
     this.id = id;
     this.object = $(id);
-    this.addOption = function(name,func){
+    this.addOption = function (name, func) {
         let obj = document.createElement('p');
         obj.innerHTML = name;
-        obj.setAttribute("name","menu");
+        obj.setAttribute("name", "menu");
         obj.onclick = func;
         obj.style.width = "fit-content";
         document.getElementById(id).appendChild(obj);
     }
-    this.removeOption = function(name){
+    this.removeOption = function (name) {
         let objs = _('menu');
-        for(i in objs){
-            if(objs[i].innerHTML == name){
+        for (i in objs) {
+            if (objs[i].innerHTML == name) {
                 objs[i].remove();
                 break;
             }
         }
     }
-    this.addEvent = function(event,name,func){
+    this.addEvent = function (event, name, func) {
         let objs = _('menu');
-        for(i in objs){
-            if(objs[i].innerHTML == name){
-                objs[i].addEventListener(event,func);
+        for (i in objs) {
+            if (objs[i].innerHTML == name) {
+                objs[i].addEventListener(event, func);
             }
         }
     }
-    this.destroy = function(){
+    this.addEventAll = function (event, func) {
         let objs = _('menu');
-        for(i in objs){
-            objs[i].remove();
+        for (let i = 0; i < objs.length; i++) {
+            objs[i].addEventListener(event, func);
         }
+    }
+    this.destroy = function () {
+        this.object.remove();
+    }
+    this.setAttribute = function (elem_text, what, attribute) {
+        let objs = _('menu');
+        for (let i = 0; i < objs.length; i++) {
+            if (objs[i].innerHTML == elem_text) {
+                objs[i].setAttribute(what, attribute);
+            }
+        }
+    }
+    this.setCSS = function(elem_text,cmd){
+        let objs = _('menu');
+        for (let i = 0; i < objs.length; i++) {
+            if (objs[i].innerHTML == elem_text) {
+                objs[i].setAttribute("style", cmd);
+            }
+        }
+    }
+    this.setL = function (x, y) {
+        init(this.object);
+        return setL(this.object, x, y);
+    }
+}
+
+// Helps You To Manage Page Better
+function pageController() {
+    this.pageWidth = window.innerWidth;
+    this.pageHeight = window.innerHeight;
+    this.changeOpacity = function (percent) {
+        document.getElementsByTagName("body")[0].style.opacity = percent / 100;
+    }
+    this.changeElementOpacity = function (id, percent) {
+        $(id).style.opacity = percent / 100;
+    }
+    this.changeElementsOpacity = function (name, percent) {
+        let d = _(name);
+        for (i in d) {
+            d[i].style.opacity = percent / 100;
+        }
+    }
+    this.backcolor = function (color) {
+        document.getElementsByTagName("body")[0].style.backgroundColor = color;
     }
 }
